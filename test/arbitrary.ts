68926aa0bfa3009dacf88e9e3d25649cbba2e3e6
Erin Millard <contact@erin.id.au>
2024-04-26 21:26:26 +1000

    Un-export internal functions

--- test/arbitrary.ts
+++ test/arbitrary.ts
@@ -4,24 +4,19 @@ import type { Vector4 } from "../src/vector.js";
 
 const RADIAN = Math.PI / 180;
 
-export function arbitraryLat(): fc.Arbitrary<number> {
-  return fc.double({
-    min: -90 * RADIAN,
-    max: 90 * RADIAN,
-    noNaN: true,
-  });
-}
-
-export function arbitraryLon(): fc.Arbitrary<number> {
-  return fc.double({
-    min: -180 * RADIAN,
-    max: 180 * RADIAN,
-    noNaN: true,
-  });
-}
-
 export function arbitraryLatLon(): fc.Arbitrary<[number, number]> {
-  return fc.tuple(arbitraryLat(), arbitraryLon());
+  return fc.tuple(
+    fc.double({
+      min: -90 * RADIAN,
+      max: 90 * RADIAN,
+      noNaN: true,
+    }),
+    fc.double({
+      min: -180 * RADIAN,
+      max: 180 * RADIAN,
+      noNaN: true,
+    }),
+  );
 }
 
 export function arbitraryQuaternion(): fc.Arbitrary<Vector4> {
