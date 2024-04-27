4340002f4eb73ff2f242521f0bf25037d38acb14
Erin Millard <contact@erin.id.au>
2024-04-27 19:24:01 +1000

    Refactor test API wrapper

--- test/nvector-test-api.ts
+++ test/nvector-test-api.ts
@@ -7,12 +7,9 @@ export type NvectorTestClient = {
     latitude: number,
     longitude: number,
     R_Ee?: Matrix3x3,
-  ) => Promise<[x: number, y: number, z: number]>;
+  ) => Promise<Vector3>;
 
-  n_E2lat_lon: (
-    n_E: Vector3,
-    R_Ee?: Matrix3x3,
-  ) => Promise<[latitude: number, longitude: number]>;
+  n_E2lat_lon: (n_E: Vector3, R_Ee?: Matrix3x3) => Promise<LatLonTuple>;
 
   close: () => void;
 };
@@ -28,24 +25,22 @@ export async function createNvectorTestClient(): Promise<NvectorTestClient> {
 
   return {
     async lat_lon2n_E(latitude, longitude, R_Ee) {
-      const [[x], [y], [z]] = await call<[[number], [number], [number]]>(
-        "lat_lon2n_E",
-        { latitude, longitude, R_Ee },
+      return unwrapVector3(
+        await call<WrappedVector3>("lat_lon2n_E", {
+          latitude,
+          longitude,
+          R_Ee,
+        }),
       );
-
-      return [x, y, z];
     },
 
-    async n_E2lat_lon([x, y, z], R_Ee) {
-      const { latitude, longitude } = await call<{
-        latitude: number;
-        longitude: number;
-      }>("n_E2lat_lon", {
-        n_E: [[x], [y], [z]],
-        R_Ee,
-      });
-
-      return [latitude, longitude];
+    async n_E2lat_lon(n_E, R_Ee) {
+      return latLonObjectToTuple(
+        await call<LatLonObject>("n_E2lat_lon", {
+          n_E: wrapVector3(n_E),
+          R_Ee,
+        }),
+      );
     },
 
     close: () => ws.close(),
@@ -88,3 +83,26 @@ type ErrorMessage = {
   error: string;
   result: undefined;
 };
+
+type LatLonTuple = [latitude: number, longitude: number];
+type LatLonObject = {
+  latitude: number;
+  longitude: number;
+};
+
+function latLonObjectToTuple({
+  latitude,
+  longitude,
+}: LatLonObject): LatLonTuple {
+  return [latitude, longitude];
+}
+
+type WrappedVector3 = [[x: number], [y: number], [z: number]];
+
+function wrapVector3([x, y, z]: Vector3): WrappedVector3 {
+  return [[x], [y], [z]];
+}
+
+function unwrapVector3([[x], [y], [z]]: WrappedVector3): Vector3 {
+  return [x, y, z];
+}
