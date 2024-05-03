3b93892f029b98c6d7521255a551584db7e72b60
Erin Millard <contact@erin.id.au>
2024-05-03 21:57:34 +1000

    Add missing links to R_EL2n_E and R_EN2n_E docs

--- src/R_EL2n_E.ts
+++ src/R_EL2n_E.ts
@@ -5,6 +5,8 @@ import type { Vector3 } from "./vector.js";
 /**
  * Finds n-vector from R_EL.
  *
+ * @see https://github.com/FFI-no/n-vector/blob/f77f43d18ddb6b8ea4e1a8bb23a53700af965abb/nvector/R_EL2n_E.m
+ *
  * @param R_EL - Rotation matrix (direction cosine matrix).
  *
  * @returns An n-vector decomposed in E.
--- src/R_EN2n_E.ts
+++ src/R_EN2n_E.ts
@@ -5,6 +5,8 @@ import type { Vector3 } from "./vector.js";
 /**
  * Finds n-vector from R_EN.
  *
+ * @see https://github.com/FFI-no/n-vector/blob/f77f43d18ddb6b8ea4e1a8bb23a53700af965abb/nvector/R_EN2n_E.m
+ *
  * @param R_EN - Rotation matrix (direction cosine matrix).
  *
  * @returns An n-vector decomposed in E.
