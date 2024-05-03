39a49887090b1d32019cd10c59bcd109d5e5a940
Erin Millard <contact@erin.id.au>
2024-05-03 21:24:14 +1000

    Implement example 10 as a test

---
+++ test/vitest/examples/example-10.spec.ts
@@ -0,0 +1,57 @@
+import { expect, test } from "vitest";
+import {
+  cross,
+  dot,
+  lat_long2n_E,
+  rad,
+  unit,
+  type Vector3,
+} from "../../../src/index.js";
+
+/**
+ * Example 10: Cross track distance (cross track error)
+ *
+ * Given path A going through A(1) and A(2), and a point B. Find the cross track
+ * distance/cross track error between B and the path.
+ *
+ * @see https://www.ffi.no/en/research/n-vector/#example_10
+ */
+test.each`
+  label                             | n_EA1_E                         | n_EA2_E                          | n_EB_E                            | s_xt_expected   | d_xt_expected
+  ${"Enter elements directly:"}     | ${unit([1, 0, -2])}             | ${unit([-1, -2, 0])}             | ${unit([0, -2, 3])}               | ${3834155.5618} | ${3606868.4923}
+  ${"or input as lat/long in deg:"} | ${lat_long2n_E(rad(0), rad(0))} | ${lat_long2n_E(rad(10), rad(0))} | ${lat_long2n_E(rad(1), rad(0.1))} | ${11117.7991}   | ${11117.7935}
+`(
+  "Example 10 ($label)",
+  // Position A1 and A2 and B are given as n_EA1_E, n_EA2_E, and n_EB_E:
+  ({
+    n_EA1_E,
+    n_EA2_E,
+    n_EB_E,
+    s_xt_expected,
+    d_xt_expected,
+  }: {
+    n_EA1_E: Vector3;
+    n_EA2_E: Vector3;
+    n_EB_E: Vector3;
+    s_xt_expected: number;
+    d_xt_expected: number;
+  }) => {
+    const r_Earth = 6371e3; // m, mean Earth radius
+
+    // Find the cross track distance from path A to position B.
+
+    // SOLUTION:
+
+    // Find the unit normal to the great circle between n_EA1_E and n_EA2_E:
+    const c_E = unit(cross(n_EA1_E, n_EA2_E));
+
+    // Find the great circle cross track distance: (acos(x) - pi/2 = -asin(x))
+    const s_xt = -Math.asin(dot(c_E, n_EB_E)) * r_Earth;
+
+    // Find the Euclidean cross track distance:
+    const d_xt = -dot(c_E, n_EB_E) * r_Earth;
+
+    expect(s_xt).toBeCloseTo(s_xt_expected, 4);
+    expect(d_xt).toBeCloseTo(d_xt_expected, 4);
+  },
+);
